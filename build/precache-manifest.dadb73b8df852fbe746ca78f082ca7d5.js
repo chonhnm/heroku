b3283286f46d6aec81b7e43b799ace86da837d10
chon <chonhnm@gmail.com>
2020-08-22 14:58:31 +0800

    add static file

---
+++ build/precache-manifest.dadb73b8df852fbe746ca78f082ca7d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82fa7a0b10e5ce8d33c5d3abd99a537f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ef0a38b76c02ae09e2c",
+    "url": "/static/css/main.2852497a.chunk.css"
+  },
+  {
+    "revision": "2c5e3e2dc8cdfbf972de",
+    "url": "/static/js/2.533e2462.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.533e2462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef0a38b76c02ae09e2c",
+    "url": "/static/js/main.102574ac.chunk.js"
+  },
+  {
+    "revision": "23bb1571825c4fcbd490",
+    "url": "/static/js/runtime-main.bd624b67.js"
+  }
+]);
--- index.js
+++ index.js
@@ -13,6 +13,7 @@ morgan.token('body', (req, res) => {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
@@ -37,7 +38,7 @@ let persons = [
         id: 4
     },
     {
-        name: "hh",
+        name: "hhh",
         number: "ll",
         id:5
     }
